7c6552be7c1799516bc6191c4139b7bfa6276416
saasey <48605836+saasey@users.noreply.github.com>
2019-03-26 08:24:11 -0400

    Update pipes.js

--- routes/pipes.js
+++ routes/pipes.js
@@ -30,6 +30,7 @@
 	//if this is designated as belonging to another pipe, it won't be passed in the url
 		if (!elem_values[i].hasAttribute("pipe") || elem_values[i].getAttribute("pipe") == elem.id)
 			elem_qstring = elem_qstring + elem_values[i].name + "=" + elem_values[i].value + "&";
+	}
 	// No 'pipe' means it is generic
 	for (var i = 0 ; i < elem_values.length ; i++) {
 	//if this is designated as belonging to another pipe, it won't be passed in the url
@@ -42,8 +43,6 @@
 				}
 			}
 		}
-	}
-
 	}
 
 	//strip last & char
